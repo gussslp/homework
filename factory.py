924d46daec167d350d37b4ecc30e79a284da5f5b
gussslp <tkachenkonikita101@gmail.com>
2022-10-04 20:15:53 +0300

    add file

--- factory.py
+++ factory.py
@@ -1,18 +1,26 @@
-class Factory:
-    def __init__(self,name, color, movie):
+class Toys:
+    def __init__(self,name, color, type):
         self.name = name
         self.color = color
-        self.movie = movie
-    
-    def buy_material(self):
-        print("buy faux fur, fabric and printed material")
+        self.type = type
 
-    def stitching(self):
-        print("material stitching")
-    
-    def paint(self):
-        print("paint material")
+    def show(self):
+        print(self.name,self.color,self.type)
 
-Factory = Factory("bear","brown","paddington bear")
+class Animals(Toys):
+    def __init__(self,name, color, type):
+        Toys.__init__(self,name, color, type)
+class Disney(Toys):
+    def __init__(self,name, color, type):
+        Toys.__init__(self,name, color, type)
 
-Factory.paint()
+def create():
+    print("buy faux fur, fabric and printed material, material stitching, paint material")
+
+toyss = [
+Animals("paddington","brown","animal"),
+Disney("Mickey mouse", "black","Disney")
+]
+for toy in toyss:
+    create()
+    toy.show()
