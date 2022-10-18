d41b7d4223d1a5192c70a5c8bb3b3c43c9c3153c
gussslp <tkachenkonikita101@gmail.com>
2022-10-18 15:28:40 +0300

    update

--- myfirstdjango/myapp/admin.py
+++ myfirstdjango/myapp/admin.py
@@ -12,7 +12,7 @@ stolb = ["id", "full_name","email","address","phone_number","current_balance", "
 db = sqlite3.connect('db.sqlite3')
 c = db.cursor() 
 
-for num in range(0,100):
+for num in range(0,1350):
     a = [json[num]["id"],json[num]["full_name"],json[num]["email"],json[num]["address"],json[num]["phone_number"],json[num]["current_balance"],json[num]["last_payment"],json[num]["reg_date"],json[num]["lastcall_date"],json[num]["status"]]
     c.execute('INSERT INTO myapp_userdata VALUES ("%s","%s","%s","%s","%s","%s","%s","%s","%s","%s")' % (a[0],a[1],a[2],a[3],a[4],a[5],a[6],a[7],a[8],a[9]))
 db.commit()
