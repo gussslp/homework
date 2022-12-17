734949aef6bdefcea2322cfe21ebdc2ca1c64d21
gussslp <tkachenkonikita101@gmail.com>
2022-12-17 20:10:38 +0200

    comm

---
+++ triangle_of_pascal.py
@@ -0,0 +1,13 @@
+from math import factorial 
+x = 300
+replace = True
+for i in range(x):
+    for b in range(x-i+1):
+        print(end=" ")
+    for b in range(i+1):
+        c = factorial(i)//(factorial(b)*factorial(i-b))
+        if replace == True:
+        	g = str(c)
+        	if c % 2 == 0:
+        		c = g.replace(g,"0")
+        	
